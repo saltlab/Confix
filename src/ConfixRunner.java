59e73450cdbeafff7a13bbe01ab95d0de5dda918
Amin Milani Fard <aminmf@ece.ubc.ca>
2014-11-18 07:52:40 -0800

    runner updated for phormer DDFs

--- src/ConfixRunner.java
+++ src/ConfixRunner.java
@@ -7,28 +7,68 @@ public class ConfixRunner {
 	private static String jsAdderess = "output/phormer_tests/phorm.js";
 	private static String scopeName = "phorm.js";
 	private static String testSuiteNameToGenerate = "tests_phormer.js";
-	
+
 	// provide input values for a DOM dependent function 
 	private static ArrayList<String> functionToTest = new ArrayList<String>();
-	
+
 	public static void main(String[] args) throws Exception {
 
 		// Testing phormer.js DDFs
-		/*functionToTest.add("toggleInfo()");
+
+		functionToTest.add("dg('test')");
+
+		functionToTest.add("toggleInfo()");
 		functionToTest.add("toggleInfo('')");
 		functionToTest.add("toggleInfo('Show')");
 		functionToTest.add("toggleInfo('Hide')");
 		functionToTest.add("toggleInfo()");
 		functionToTest.add("toggleInfo()");
 		functionToTest.add("toggleInfo()");
-		*/
-		functionToTest.add("dg('test')");
-		functionToTest.add("NewSaveRating(1,3)");
-		functionToTest.add("sumTotalPrice()");
-		functionToTest.add("SaveRating(1,3)");
+
+		functionToTest.add("reToggleInfo()");
+
+		functionToTest.add("reshuffle()");
+
+		functionToTest.add("updateIndic()");
+
+		functionToTest.add("SaveRating(0,1)");
+		functionToTest.add("SaveRating(0,0)");
+
+		functionToTest.add("prepareBody()");
+
+		functionToTest.add("hideElem('id')");
+
+		functionToTest.add("showElem('id')");
+
+		functionToTest.add("inlineElem('id')");
+
+		functionToTest.add("tableRowElem('id')");
+
+		functionToTest.add("checkWV()");
+
+		functionToTest.add("doReply(0)");
+
+		functionToTest.add("doReply(1)");
+
+		functionToTest.add("ss_next()");
+
+		functionToTest.add("ss_prev()");
+
+		functionToTest.add("ss_update()");
+
+		functionToTest.add("ss_playpause()");
+
+		functionToTest.add("ss_toggleSmaller()");
+
+		functionToTest.add("ss_run()");
+
+		functionToTest.add("ss_slideshow()");
+
+		//functionToTest.add("sumTotalPrice()");
 
 		ConcolicEngine ce = new ConcolicEngine(jsAdderess, scopeName, functionToTest, testSuiteNameToGenerate);
 		ce.run();
+		//ce.generateCoverageReport();
 	}
 
 
