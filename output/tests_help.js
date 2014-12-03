3e7b6d6733c228cb41216f2b23a352222e54f787
Amin Milani Fard <aminmf@ece.ubc.ca>
2014-12-03 13:34:56 -0800

    organizing output

---
+++ output/tests_help.js
@@ -0,0 +1,392 @@
+// QUnit test cases generated @ Wed Dec 03 09:32:40 PST 2014
+
+module("tests_help.js", {
+    setup: function() {
+        // generate fixture before each test
+		//$("#qunit-fixture").append('${fixture}');
+    },
+    teardown: function() {
+        // runs after each test
+		//${testTearDownCode}
+    }
+});
+
+
+
+test("Testing function HideHelp() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append(' <div id="helpBox"/> ');
+	HideHelp();
+});
+
+
+test("Testing function dgp('id') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append(' <div id="id"/> ');
+	dgp('id');
+});
+
+
+test("Testing function ShowHelp('test', null, 'it') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append(' <div id="helpBoxLegend"/> ');
+	ShowHelp('test', null, 'it');
+});
+
+
+test("Testing function ShowHelp('test', null, 'test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append(' <div id="helpBoxLegend"/> ');
+	ShowHelp('test', null, 'test');
+});
+
+
+test("Testing function SwitchSelectIE() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	SwitchSelectIE();
+});
+
+
+test("Testing function setExif('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	setExif('test');
+});
+
+
+test("Testing function imageUploaded(false, 'test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	imageUploaded(false, 'test');
+});
+
+
+test("Testing function imageUploaded(true, 'test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	imageUploaded(true, 'test');
+});
+
+
+test("Testing function writeYet('test', false) for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	writeYet('test', false);
+});
+
+
+test("Testing function writeYet('test', true) for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	writeYet('test', true);
+});
+
+
+test("Testing function uploadSubmitted('test', 'test', false, 'test')  for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	uploadSubmitted('test', 'test', false, 'test') ;
+});
+
+
+test("Testing function uploadSubmitted('test', 'test', true, 'test')  for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	uploadSubmitted('test', 'test', true, 'test') ;
+});
+
+
+test("Testing function PrepareBody() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	PrepareBody();
+});
+
+
+test("Testing function dg('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	dg('test');
+});
+
+
+test("Testing function hideElem('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	hideElem('test');
+});
+
+
+test("Testing function showElem('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	showElem('test');
+});
+
+
+test("Testing function inlineElem('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	inlineElem('test');
+});
+
+
+test("Testing function tableRowElem('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	tableRowElem('test');
+});
+
+
+test("Testing function checkChangePass() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	checkChangePass();
+});
+
+
+test("Testing function checkInstallPass() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	checkInstallPass();
+});
+
+
+test("Testing function checkPrivacyRow() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	checkPrivacyRow();
+});
+
+
+test("Testing function checkHasPass() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	checkHasPass();
+});
+
+
+test("Testing function checkDate() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	checkDate();
+});
+
+
+test("Testing function CheckAddPhotoTime() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	CheckAddPhotoTime();
+});
+
+
+test("Testing function CheckAddPhoto() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	CheckAddPhoto();
+});
+
+
+test("Testing function ConfirmDelPhoto() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	ConfirmDelPhoto();
+});
+
+
+test("Testing function showlinkline('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	showlinkline('test');
+});
+
+
+test("Testing function hidelinkline('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	hidelinkline('test');
+});
+
+
+test("Testing function linkAddBelow('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	linkAddBelow('test');
+});
+
+
+test("Testing function linkDelThis('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	linkDelThis('test');
+});
+
+
+test("Testing function removeThisNode('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	removeThisNode('test');
+});
+
+
+test("Testing function addMainColDiv() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	addMainColDiv();
+});
+
+
+test("Testing function updateMode() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	updateMode();
+});
+
+
+test("Testing function fixBoldInput('test','test1') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	fixBoldInput('test','test1');
+});
+
+
+test("Testing function changePrev() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	changePrev();
+});
+
+
+test("Testing function rethumb_fill('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	rethumb_fill('test');
+});
+
+
+test("Testing function rethumb() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	rethumb();
+});
+
+
+test("Testing function ToggleAdvPref() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	ToggleAdvPref();
+});
+
+
+test("Testing function updateTimeDiffer('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	updateTimeDiffer('test');
+});
+
+
+test("Testing function CheckDateDrafts() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	CheckDateDrafts();
+});
+
+
+test("Testing function CheckActionDrafts() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	CheckActionDrafts();
+});
+
+
+test("Testing function updateSelCount() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	updateSelCount();
+});
+
+
+test("Testing function DraftsSelectBit('test','test1') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	DraftsSelectBit('test','test1');
+});
+
+
+test("Testing function AddAddBox() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	AddAddBox();
+});
+
+
+test("Testing function SaveRatio() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	SaveRatio();
+});
+
+
+test("Testing function MouseDown('test','test1') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	MouseDown('test','test1');
+});
+
+
+test("Testing function MouseDownTheSkeleton('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	MouseDownTheSkeleton('test');
+});
+
+
+test("Testing function ExpandSkl() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	ExpandSkl();
+});
+
+
+test("Testing function MouseMoveInside('test') for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	MouseMoveInside('test');
+});
+
+
+test("Testing function UpdateThumbPrev() for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	UpdateThumbPrev();
+});
+
+
+test("Testing function () for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	();
+});
+
+
+test("Testing function () for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	();
+});
+
+
+test("Testing function () for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	();
+});
+
+
+test("Testing function () for path 1", function() {
+	expect(0);
+	$("#qunit-fixture").append('');
+	();
+});
+
